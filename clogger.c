484dc0f370507d85c514c513403d647dc99ee48c
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-01-20 08:57:52 -0300

    typo fix

--- clogger.c
+++ clogger.c
@@ -7,8 +7,8 @@
 #include <string.h>
 #include <time.h>
 
-typedef void (*to_file_fn)(FILE *stream, const char *time, LogType type, const char *filename, const char *function, int line, const char *format, va_list args);
-typedef void (*to_console_fn)(const char *time, LogType type, const char *filename, const char *function, int line, const char *format, va_list args);
+typedef void (*to_file_fn)(FILE *stream, const char *time, Log_Type type, const char *filename, const char *function, int line, const char *format, va_list args);
+typedef void (*to_console_fn)(const char *time, Log_Type type, const char *filename, const char *function, int line, const char *format, va_list args);
 
 typedef struct Clogger Clogger;
 static struct Clogger {
@@ -64,16 +64,16 @@ clogger_log(FILE *stream, const char *time, const char *log_level, const char *f
 }
 
 static char *
-clogger_type_string(LogType type, bool colored)
+clogger_type_string(Log_Type type, bool colored)
 {
         switch (type) {
-        case LOG_INFO:
+        case Log_INFO:
                 return colored ? "\x1B[32mINFO\033[0m " : "INFO ";
-        case LOG_DEBUG:
+        case Log_DEBUG:
                 return colored ? "\x1B[34mDEBUG\033[0m" : "DEBUG";
-        case LOG_WARN:
+        case Log_WARN:
                 return colored ? "\x1B[33mWARN\033[0m " : "WARN ";
-        case LOG_ERROR:
+        case Log_ERROR:
                 return colored ? "\x1B[31mERROR\033[0m" : "ERROR";
         default:
                 return "UNDEF";
@@ -81,13 +81,13 @@ clogger_type_string(LogType type, bool colored)
 }
 
 static void
-log_to_file(FILE *stream, const char *time, LogType type, const char *filename, const char *function, int line, const char *format, va_list args)
+log_to_file(FILE *stream, const char *time, Log_Type type, const char *filename, const char *function, int line, const char *format, va_list args)
 {
         clogger_log(stream, time, clogger_type_string(type, false), filename, function, line, format, args);
 }
 
 static void
-log_to_console(const char *time, LogType type, const char *filename, const char *function, int line, const char *format, va_list args)
+log_to_console(const char *time, Log_Type type, const char *filename, const char *function, int line, const char *format, va_list args)
 {
         clogger_log(_logger.console_stream, time, clogger_type_string(type, _logger.colored), filename, function, line, format, args);
 }
@@ -133,7 +133,7 @@ get_time(char *dest, size_t dest_size)
 }
 
 void
-clogger(LogType type, const char *filename, const char *function, int line, const char *format, ...)
+clogger(Log_Type type, const char *filename, const char *function, int line, const char *format, ...)
 {
         if (!_logger.initialized) {
                 clogger_init();
@@ -157,7 +157,7 @@ clogger(LogType type, const char *filename, const char *function, int line, cons
 }
 
 void
-clogger_f(LogType type, FILE *stream, const char *filename, const char *function, int line, const char *format, ...)
+clogger_f(Log_Type type, FILE *stream, const char *filename, const char *function, int line, const char *format, ...)
 {
         if (!_logger.initialized) {
                 clogger_init();
--- clogger.h
+++ clogger.h
@@ -9,31 +9,25 @@
 #define ACTIVE   true
 #define INACTIVE false
 
-typedef enum LogType {
-        LOG_INFO,
-        LOG_DEBUG,
-        LOG_WARN,
-        LOG_ERROR
-} LogType;
+typedef enum Log_Type {
+        Log_INFO,
+        Log_DEBUG,
+        Log_WARN,
+        Log_ERROR
+} Log_Type;
+
+#define LOG_INFO(FORMAT, ...) clogger(Log_INFO, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
+#define LOG_DEBUG(FORMAT, ...) clogger(Log_DEBUG, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
+#define LOG_WARNING(FORMAT, ...) clogger(Log_WARN, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
+#define LOG_ERROR(FORMAT, ...) clogger(Log_ERROR, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
+
+#define LOG_INFO_F(STREAM, FORMAT, ...) clogger_f(Log_INFO, STREAM, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
+#define LOG_DEBUG_F(STREAM, FORMAT, ...) clogger_f(Log_DEBUG, STREAM, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
+#define LOG_WARNING_F(STREAM, FORMAT, ...) clogger_f(Log_WARN, STREAM, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
+#define LOG_ERROR_F(STREAM, FORMAT, ...) clogger_f(Log_ERROR, STREAM, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
 
 void clogger_colored_console(bool status);
-void clogger(LogType type, const char *filename, const char *function, int line, const char *format, ...);
-void clogger_f(LogType type, FILE *stream, const char *filename, const char *function, int line, const char *format, ...);
-
-#define LOG_INFO(FORMAT, ...) clogger(LOG_INFO, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
-
-#define LOG_INFO_F(STREAM, FORMAT, ...) clogger_f(LOG_INFO, STREAM, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
-
-#define LOG_DEBUG(FORMAT, ...) clogger(LOG_DEBUG, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
-
-#define LOG_DEBUG_F(STREAM, FORMAT, ...) clogger_f(LOG_DEBUG, STREAM, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
-
-#define LOG_WARNING(FORMAT, ...) clogger(LOG_WARN, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
-
-#define LOG_WARNING_F(STREAM, FORMAT, ...) clogger_f(LOG_WARN, STREAM, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
-
-#define LOG_ERROR(FORMAT, ...) clogger(LOG_ERROR, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
-
-#define LOG_ERROR_F(STREAM, FORMAT, ...) clogger_f(LOG_ERROR, STREAM, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
+void clogger(Log_Type type, const char *filename, const char *function, int line, const char *format, ...);
+void clogger_f(Log_Type type, FILE *stream, const char *filename, const char *function, int line, const char *format, ...);
 
 #endif
