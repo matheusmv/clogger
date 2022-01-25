97d28e2caab080724b55eb0c57fb55b96fa889e3
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-01-25 11:05:23 -0300

    add NCLOG identifier to disable logs in console

--- clogger.h
+++ clogger.h
@@ -12,10 +12,17 @@ typedef enum log_type {
         ERROR
 } log_type_t;
 
+#if defined(NCLOG)
+#define LOG_INFO(FORMAT, ...)
+#define LOG_DEBUG(FORMAT, ...)
+#define LOG_WARNING(FORMAT, ...)
+#define LOG_ERROR(FORMAT, ...)
+#else
 #define LOG_INFO(FORMAT, ...) clogger(INFO, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
 #define LOG_DEBUG(FORMAT, ...) clogger(DEBUG, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
 #define LOG_WARNING(FORMAT, ...) clogger(WARN, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
 #define LOG_ERROR(FORMAT, ...) clogger(ERROR, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
+#endif /* NCLOG */
 
 #define LOG_INFO_F(FILEPATH, FORMAT, ...) clogger_f(INFO, FILEPATH, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
 #define LOG_DEBUG_F(FILEPATH, FORMAT, ...) clogger_f(DEBUG, FILEPATH, __FILE__, __func__, __LINE__, FORMAT, ##__VA_ARGS__)
@@ -27,4 +34,4 @@ void clogger(log_type_t type, const char *filename, const char *function,
 void clogger_f(log_type_t type, const char *filepath, const char *filename,
                const char *function, int line, const char *format, ...);
 
-#endif
+#endif /* CLOGGER_H */
